1ae53c4366336db4ab92a88c12dcd33802b06fd7
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-10-25 11:08:50 -0400

    Added code to populate my account page

--- js/myaccount.js
+++ js/myaccount.js
@@ -5,3 +5,19 @@ if (loc1 == -1)
 {
     window.location.href = "index.html";
 }
+loc1 = cook.indexOf("=", loc1 + 1);
+var useraccountstr = "";
+var loc2 = cook.indexOf(";", loc1 + 1);
+if (loc2 == -1)
+{
+    useraccountstr = cook.substring(loc1 + 1, cook.length);
+}
+else
+{
+    useraccountstr = cook.substring(loc1 + 1, loc2);
+}
+var account = JSON.parse(useraccountstr);
+
+//Populate the page with the properties
+document.getElementById("username").innerHTML = account.Username;
+document.getElementById("email").innerHTML = account.Email;
