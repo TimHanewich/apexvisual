def3f4c9c82f5594f770af32b7a155d9dae48463
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-10-24 21:39:01 -0400

    Added js for mouse over and mouse exit on the react button

---
+++ js/login.js
@@ -0,0 +1,16 @@
+function enter_login()
+{
+    var rb = document.getElementById("reactbutton");
+    rb.setAttribute("class", "rb-on");
+}
+
+function exit_login()
+{
+    var rb = document.getElementById("reactbutton");
+    rb.setAttribute("class", "rb-off");
+}
+
+function login()
+{
+    
+}
