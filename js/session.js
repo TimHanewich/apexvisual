77dcaf5d21f0f600081c884b1114a7b7cd2b5eb2
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-10-28 23:36:15 -0400

    Added code for requesting session summary from API service.

--- js/session.js
+++ js/session.js
@@ -8,10 +8,23 @@ if (sess == null)
     
     //Remove the hidden class from the error
     document.getElementById("sessionidnullerr").classList.remove("hidden");
+
+    
 }
 
 //Sent it to the API
-
+var dest_url = "https://apexvisual2020.azurewebsites.net/api/GetSessionSummary?id=" + sess;
+var req = new XMLHttpRequest();
+req.open("GET", dest_url);
+req.onreadystatechange = function()
+{
+    if (req.readyState == 4 && req.status == 200)
+    {
+        var as_json = JSON.parse(req.responseText);
+        console.log(as_json.CircuitString);
+    }
+}
+req.send();
 
 
 
