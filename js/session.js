92a30fb11b144f78d33bcc6466dd40e3f490ab23
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-10-28 23:27:39 -0400

    Added loading and error page to session page

--- js/session.js
+++ js/session.js
@@ -1,3 +1,19 @@
+//Get the session id from the URL
 var this_url = new URL(window.location.href);
-var sess = this_url.searchParams.get("session");
-console.log(sess);
+var sess = this_url.searchParams.get("sessionid");
+if (sess == null)
+{
+    //Hide the loading session pane
+    document.getElementById("loading_cover").classList.add("hidden");
+    
+    //Remove the hidden class from the error
+    document.getElementById("sessionidnullerr").classList.remove("hidden");
+}
+
+//Sent it to the API
+
+
+
+
+
+////UTILITY FUNCTIONS
