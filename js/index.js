b75a02e9add34f8cda962f0d5bd3cf5ebd1e1a9a
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-11-01 22:03:37 -0500

    JS for reacting to mouse over in the tabs

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function BoldTab(tab_name)
+{
+    document.getElementById(tab_name).classList.add("bolded");
+}
+
+function UnBoldTab(tab_name)
+{
+    document.getElementById(tab_name).classList.remove("bolded");
+}
