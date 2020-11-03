a78090e50867395be91c8a28c6467818261dc88d
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-11-02 21:16:36 -0500

    Toolkit

---
+++ js/toolkit.js
@@ -0,0 +1,17 @@
+function FriendlyLapTime(lap_time)
+{
+    if (lap_time < 60)
+    {
+        return lap_time.toString();
+    }
+    else
+    {
+        var div = lap_time / 60;
+        var mins = Math.floor(div);
+        var remainderpercentage = div - mins;
+        var secs = remainderpercentage * 60;
+        secs = Math.round(secs * 1000) / 1000;
+        var str = mins.toString() + ":" + secs.toString();
+        return str;
+    }
+}
